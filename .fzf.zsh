137246ce9fa4d3241aa4f13bbd35f0c4bedcbe0a
BD <baisalbek.daniiarov@gmail.com>
2025-02-28 01:32:38 +0600

    Create .fzf.zsh

---
+++ .fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */c/Users/xbaysal11/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/c/Users/xbaysal11/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+# source "/c/Users/xbaysal11/.fzf/shell/completion.zsh"
+
+# Key bindings
+# ------------
+# source "/c/Users/xbaysal11/.fzf/shell/key-bindings.zsh"
